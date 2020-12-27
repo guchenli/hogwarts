d27491aac648a23c31321a4c57a0279e1a235485
guchenli <295532986@qq.com>
2020-12-27 15:10:39 +0800

    12/27

--- automation/web_automation/base.py
+++ automation/web_automation/base.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
+import os
 import time
 
 from selenium import webdriver
 
-
 class Base:
     def setup(self):
-        self.driver = webdriver.Chrome()
-        self.driver.maximize_window()
+        #方法返回环境变量键的值
+        #browser=firefox pytest + 文件名
+        browser = os.getenv("browser")
+        if browser == "firefox":
+            self.driver = webdriver.Firefox()
+        elif browser == "headless":
+            self.driver = webdriver.PhantomJS
+        else:
+            self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(10)
+        self.driver.maximize_window()
+
 
     def teardown(self):
         time.sleep(3)
